5fec9f7b8ddf447d0b768be8c9d5d91be516d25e
Dzaki106 <Zmubarak746@gmail.com>
2024-10-09 15:49:43 +0700

    Add files via upload

---
+++ pertemuan 6/2409106106_DzakiMubarak_C1'24_Pertemuan6.py
@@ -0,0 +1,123 @@
+# daftar_buku = {
+# "Buku1" : "Harry Potter",
+# "Buku2" : "Percy Jackson",
+# "Buku3" : "Twillight"
+# }
+# print(daftar_buku["Buku1"])
+# print(daftar_buku["Buku2"])
+# print(daftar_buku["Buku3"])
+
+
+# Biodata = {
+# "Nama" : "Aldy Ramadhan Syahputra",
+# "NIM" : 2109106079,
+# "KRS" : ["Program Web", "Struktur Data", "Basis Data"],
+# "Mahasiswa_Aktif" :True,
+# "Social Media" : {
+# "Instagram" : "@aldyrmdhns_",
+# "Discord" : "\'Izanami#6848"
+# "Email" : "Iniemail@gmail.com"
+# }
+# }
+
+# print(Biodata["KRS"][0])
+# print(Biodata["Social Media"]["Email"])
+
+# games = dict(Sekiro = "Action", Pokemon = "Adventure",
+# Valorant = {"nama" :}{123 : "informatika"})
+# print(games["valorant"]['nama'][123])
+
+# Game = {
+#     "Game1" : "Pubg mobile",
+#     "Game2" : " mobile legend",
+#     "Game3" : {
+#        "nama" : "coc",
+#        "genre" : "Strategy"
+#     }
+# }
+
+# print ((Game get))
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81,
+# "Kimia" : 78,
+# "Fisika" : 80
+# }
+
+# #tanpa menggunakan items
+# for i in Nilai:
+#    print(i)
+# print("")
+# #menggunakan items
+# for mapel,nilai in Nilai.items():
+#    print(f"Nilai {mapel} anda adalah {nilai}")
+
+# Film = {
+# "Avenger Endgame" : "Action",
+# "Sherlock Holmes" : "Mystery",
+# "The Conjuring" : "Horror"
+# }
+# #Sebelum Ditambah
+# print(Film)
+
+# Film["Zombieland"] = "Comedy"
+# Film.update({"Hours" : "Thriller,
+#              "Rush Hour" : "comedy", 
+#              "Oblivion" : "Science Fiction"})
+# #Setelah Ditambah
+# print(Film)
+
+# Penggunaan del
+# del film['Avenger Endgame']
+
+# simpan = Film.pop('hours')
+# film.clear()
+# print(Film)
+# Film["Hours"] = simpan
+# print(Film)
+# print("Jumlah Film = ", len(Film))
+
+# Movies = film.copy()
+
+# key = "apel", "jeruk", "mangga"
+# value = 1
+# buah = dict.fromkeys(key, value)
+# print(buah)
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81,
+# "Kimia" : 78,
+# "Fisika" : 80
+# }
+# #menggunakan keys
+# for i in Nilai.keys():
+#   print(i)
+# print("")
+# #menggunakan value
+# for i in Nilai.values():
+#    print(i)
+
+# Musik = {
+# "The Chainsmoker" : ["All we Know", "The
+# Paris"],
+# "Alan Walker" : ["Alone", "Lily"],
+# "Neffex" : ["Best of Me", "Memories"]
+# }
+# f          or i, j in Musik.items():
+#     rint(f"Musik milik {i} adalah : ")
+# for song in j:
+#       rint(song)
+# print("") 
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18}
+# }
+# #Sebelum Dilakukan Perubahan
+# print(mahasiswa)
+
+
